a50ad7d9bc0797d7e49a1b96e4ca9a29a4b32fac
Asir <vojt.tieg295i@gmail.com>
2023-01-31 14:31:54 +0200

    deploy

--- src/core/settings.py
+++ src/core/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hdjnn6&2$ca^)++xlbl(q&j0kns%t3gp*o*164sd_3r3j%aba1'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")#
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DJANGO_DEBUG", default=1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -84,27 +84,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql', 
-#         'NAME': os.getenv('POSTGRES_POSTGNAME'),
-#         'USER': os.getenv('POSTGRES_USER'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-#         'HOST': os.getenv('POSTGRES_HOST'),
-#         'PORT': os.getenv('POSTGRES_PORT'),
-#     }
-# }
-
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql', 
-        'NAME': os.getenv('POSTGRES_DOCKER_NAME'),
-        'USER': os.getenv('POSTGRES_DOCKER_USER'),
-        'PASSWORD': os.getenv('POSTGRES_DOCKER_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_DOCKER_HOST'),
-        'PORT': os.getenv('POSTGRES_DOCKER_PORT'),
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'), 
+        'NAME': os.environ.get('DB_NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('DB_USER', 'user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
