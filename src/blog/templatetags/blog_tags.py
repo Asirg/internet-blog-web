756a93484d0ecf8f35a7e71a2fcc1542679ad792
Asir <vojt.tieg295i@gmail.com>
2023-02-11 09:58:19 +0200

    add django-filters and refactor

--- src/blog/templatetags/blog_tags.py
+++ src/blog/templatetags/blog_tags.py
@@ -1,12 +1,14 @@
+from django.db.models import QuerySet
 from django import template
+from typing import Dict, Union
 
-from blog.models import Category, Post
+from blog.models import Category, Post, Tag
 
 
 register = template.Library()
 
 @register.simple_tag()
-def link_page_with_args(page, args):
+def link_page_with_args(page: int, args: dict) -> str:
     link = f"?page={page}&"
     if args:
         for key, value in args.items():
@@ -21,5 +23,15 @@ def link_page_with_args(page, args):
     return link
 
 @register.simple_tag()
-def get_main_categories():
+def get_main_categories() -> QuerySet[Category]:
     return Category.objects.filter(parent__isnull=True)
+
+@register.simple_tag()
+def get_popular_posts(category: Category, count: int = 5) -> QuerySet[Post]:
+    return Post.objects\
+            .filter(categories__in=category.get_sub_categories)\
+            .order_by('-number_of_views').distinct()[:count]
+
+@register.simple_tag()
+def get_popular_tags(category: Category, count: int =10) -> QuerySet[Tag]:
+    return category.get_popular_tags[:count]
--- src/core/settings.py
+++ src/core/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
-
+    'django_filters',
     'ckeditor',
     'ckeditor_uploader',
     'blog',
