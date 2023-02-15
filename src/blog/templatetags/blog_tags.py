84ffc66488bd5c37c7052ec33213f73210b0f8f0
Asir <vojt.tieg295i@gmail.com>
2023-02-15 07:34:39 +0200

    drop db

--- src/blog/templatetags/blog_tags.py
+++ src/blog/templatetags/blog_tags.py
@@ -26,21 +26,27 @@ def link_page_with_args(page: int, args: dict) -> str:
 def get_main_categories() -> QuerySet[Category]:
     return Category.objects.filter(parent__isnull=True)
 
+def filter_by_category(queryset, category):
+    if category:
+        try:
+            category = Category.objects.get(name=category)
+            queryset = queryset.filter(categories__in=category.get_sub_categories).distinct()
+        except Category.objects.model.DoesNotExist:
+            return []
+    return queryset
+
 @register.simple_tag()
 def get_last_posts(category: str = "", count: int = 5) -> QuerySet[Post]:
     queryset = Post.objects.all().order_by('-publication_date')
-    if category:
-        category = Category.objects.get(name=category)
-        queryset = queryset.filter(categories__in=category.get_sub_categories).distinct()
+    queryset = filter_by_category(queryset, category)
     return queryset[:count]
 
 @register.simple_tag()
 def get_popular_posts(category: str = '', count: int = 5) -> QuerySet[Post]:
     queryset = Post.objects.all().order_by('-number_of_views')
-    if category:
-        category = Category.objects.get(name=category)
-        queryset = queryset.filter(categories__in=category.get_sub_categories).distinct()
+    queryset = filter_by_category(queryset, category)
     return queryset[:count]
+    
 
 @register.simple_tag()
 def get_popular_tags(category: Category, count: int =10) -> QuerySet[Tag]:
